f2d6a80f6d7cdf939f70143be960c5532b40cc33
David <davidjsupoo@gmail.com>
2019-04-09 14:00:28 -0500

    Learn routing

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html'
+})
+export class HeroeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  verHeroe(idx:number){
+    console.log(idx);
+  }
+
+}
